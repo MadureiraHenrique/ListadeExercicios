f0916386222205efb81c2405e5a25d8a6a875911
CARLOS HENRIQUE MADUREIRA SANTOS <200037374@UCSAL.BR>
2025-08-14 16:52:27 -0300

    Questão 9

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -3,43 +3,32 @@ package application;
 import java.util.Scanner;
 import java.util.Locale;
 
-// 8) A escola “APRENDER” faz o pagamento de seus professores por hora/aula. Faça um programa em Java que
-// receba o enquadramento do professor e a quantidade de horas trabalhadas, calcule e exiba o salário do
-// professor. Sabe-se que o valor da hora/aula segue a tabela abaixo:
+// 9) Faça um programa que receba um valor n e em seguida informe o valor de S.
+// a) s=1+1/2+1/3 +...+ 1/n
+// b) s=1/n + 2/(n-1) + 3/(n-2) +...+ (n-1)/2+n
+// c) s=1 - 1/2 + 1/3 - 1/4 +...+ 1/n
 
 public class Questions {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-        System.out.print("Qual o seu Nivel? ");
-        int nivel = scan.nextInt();
 
-        System.out.print("\nQuantas Horas Trabalhas? ");
-        int horas = scan.nextInt();
+        System.out.println("Qual o valor de n?");
+        int i = scan.nextInt();
 
-        System.out.println("\nValor Total: " + valorTotal(horas, enquadramento(nivel)));
+        double[] s = new double[3];
+
+        for (int n = 1; n < i; n++) {
+            s[0] += (1 + 1) / n;
+            s[1] += (n - 1) / 2 + n;
+            s[2] += 1 / n;
+        }
+
+        System.out.println("\nResultado");
+        System.out.println("a) " + String.format("%.2f", s[0]));
+        System.out.println("b) " + String.format("%.2f", s[1]));
+        System.out.println("c) " + String.format("%.2f", s[2]));
 
         scan.close();
-    }
-
-    // Enquadramento Valor da hora/aula
-    // Nível 1 R$12,00
-    // Nível 2 R$17,00
-    // Nível 3 R$25,00
-    public static int enquadramento(int nivel) {
-        if (nivel == 1) {
-            return (12);
-        } else if (nivel == 2) {
-            return (17);
-        } else if (nivel == 3) {
-            return (25);
-        } else {
-            System.out.println("Nivel Inválido");
-            return (0);
-        }
-    }
-
-    public static double valorTotal(int horasTrabalhadas, int valorNivel) {
-        return horasTrabalhadas * valorNivel;
     }
 
 }
