3f3016bc22fb4272ca53e8eff9dfb7d2810e384b
CARLOS HENRIQUE MADUREIRA SANTOS <200037374@UCSAL.BR>
2025-08-14 15:31:48 -0300

    Questão 6

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -1,32 +1,26 @@
 package application;
 
-import java.text.ParseException;
 import java.util.Scanner;
 import java.util.Locale;
 
-// 5) Faça um algoritmo em Java que receba a idade de uma pessoa e informe em qual das categorias se
-// enquadra em relação à obrigatoriedade do voto:
-// • Sem permissão para votar (0 a 15 anos)
-// • Voto facultativo (16 a 17 anos, acima de 65 anos)
-// • Voto obrigatório (18 a 64 anos)
+// 6) As maçãs custam R$ 0,30 cada se forem compradas menos do que uma dúzia, e R$ 0,25 se forem
+// compradas pelo menos doze. Escreva um programa que leia o número de maçãs compradas, calcule e
+// escreva o valor total da compra.
 
 public class Questions {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-        System.out.print("Idade:");
-        System.out.println(obrigatoriedade(scan.nextInt()));
+        System.out.print("Quantidade de Maçãs: ");
+        int quantidadeDeMacas = scan.nextInt();
+        System.out.println("Valor Total da Compra: " + );
         scan.close();
     }
 
-    public static String obrigatoriedade(int idade) {
-        if (idade > 0 && idade <= 15) {
-            return ("Sem permissão para votar!");
-        } else if (idade > 15 && idade <= 17 || idade > 64) {
-            return ("Voto facultativo!");
-        } else if (idade >= 18 && idade <= 64) {
-            return ("Voto obrigatório!");
+    public static double valorTotal() {
+        if () {
+            return 
         } else {
-            return ("Idade Inexistente");
+            return 
         }
     }
 
