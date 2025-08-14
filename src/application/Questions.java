dc6557fc4cbf5da3eb6086d07ccd87608aee9f62
CARLOS HENRIQUE MADUREIRA SANTOS <200037374@UCSAL.BR>
2025-08-14 14:20:06 -0300

    Questão 4

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -1,35 +1,38 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
-// 3) Faça um programa em Java que receba 3 notas de um aluno e seus respectivos pesos, calcule e informe a
-// média ponderada.
+// 4) Faça um programa em Java que receba as horas de início e fim de uma atividade (expressas em horas,
+// minutos e segundos) e ao final informe o tempo de duração da atividade. Dica: transforme as horas de início
+// e fim em segundos, realize a subtração e em seguida volte a transformar em horas, minutos e segundos.
 
 public class Questions {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-        List<Double> notas = new ArrayList<>();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Nota " + (i + 1) + ": ");
-            notas.add(scan.nextDouble());
-            System.out.println();
-        }
+        System.out.println("Modelo: hora:minuto:segundo");
+        System.out.print("Digite Hora, Minuto e Segundo Inicias: ");
+        String tempo = scan.nextLine();
 
-        System.out.printf("Media: " + String.format("%.2f", media(notas)));
+        LocalTime tempoInicial = LocalTime.parse(tempo, dtf);
+
+        System.out.print("Digite Hora, Minuto e Segundo Finais: ");
+        tempo = scan.nextLine();
+
+        LocalTime tempoFinal = LocalTime.parse(tempo, dtf);
+
+        Duration duracao = Duration.between(tempoInicial, tempoFinal);
+
+        System.out.println("Duração entre " + tempoInicial + " e " + tempoFinal + ":\n"
+                + duracao.toHours() + ":" + duracao.toMinutesPart() + ":" + duracao.toSecondsPart());
 
         scan.close();
-    }
-
-    public static double media(List<Double> nota) {
-        double valorDaMedia = 0.0;
-        for (Double notas : nota) {
-            valorDaMedia += notas;
-        }
-        return valorDaMedia / 3;
     }
 
 }
