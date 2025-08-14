2ea9f6673239f52fbffd45bfba6ea68bedf72f84
CARLOS HENRIQUE MADUREIRA SANTOS <200037374@UCSAL.BR>
2025-08-14 12:06:06 -0300

    Questão 2

--- src/application/questao1.java
+++ src/application/questao1.java
@@ -1,20 +1,27 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-// 1) Faça um programa em Java que receba uma temperatura em centígrados e transforme para fahrenheit.
-// Sabe-se que a fórmula para conversão é F=((9.C)+160)/5.
+// 2) Faça um programa em Java que solicite a hora atual (expressa em horas, minutos e segundos) ao usuário e
+// informe quantos segundos do dia já passaram.
 
 public class questao1 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite o valor em Graus:");
-        double fahrenheit = scan.nextDouble();
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("Digite as Horas: ");
+        int horas = scan.nextInt(); 
 
-        System.out.print("O valor total é: " + calculo(fahrenheit));
-    }
+        System.out.print("Digite os Minutos: ");
+        int minutos = scan.nextInt();
+        minutos += (horas * 60);
 
-    public static double calculo(double fahrenheit) {
-        return ((9 * fahrenheit) + 160) / 5;
+        System.out.print("Digite os Segundos: ");
+        int segundos = scan.nextInt();
+
+        double totalDeSegundos = (minutos * 60) + segundos;
+
+        System.out.println("O Total de Segundos é: " + String.format("%.2f", totalDeSegundos));
+
     }
 }
