735e4f50441ba3c5f97a4023a933ccec1c939359
CARLOS HENRIQUE MADUREIRA SANTOS <200037374@UCSAL.BR>
2025-08-14 14:43:46 -0300

    Questão 5

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -1,38 +1,33 @@
 package application;
 
 import java.text.ParseException;
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 import java.util.Scanner;
+import java.util.Locale;
 
-// 4) Faça um programa em Java que receba as horas de início e fim de uma atividade (expressas em horas,
-// minutos e segundos) e ao final informe o tempo de duração da atividade. Dica: transforme as horas de início
-// e fim em segundos, realize a subtração e em seguida volte a transformar em horas, minutos e segundos.
+// 5) Faça um algoritmo em Java que receba a idade de uma pessoa e informe em qual das categorias se
+// enquadra em relação à obrigatoriedade do voto:
+// • Sem permissão para votar (0 a 15 anos)
+// • Voto facultativo (16 a 17 anos, acima de 65 anos)
+// • Voto obrigatório (18 a 64 anos)
 
 public class Questions {
     public static void main(String[] args) throws ParseException {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        System.out.println("Modelo: hora:minuto:segundo");
-        System.out.print("Digite Hora, Minuto e Segundo Inicias: ");
-        String tempo = scan.nextLine();
-
-        LocalTime tempoInicial = LocalTime.parse(tempo, dtf);
-
-        System.out.print("Digite Hora, Minuto e Segundo Finais: ");
-        tempo = scan.nextLine();
-
-        LocalTime tempoFinal = LocalTime.parse(tempo, dtf);
-
-        Duration duracao = Duration.between(tempoInicial, tempoFinal);
-
-        System.out.println("Duração entre " + tempoInicial + " e " + tempoFinal + ":\n"
-                + duracao.toHours() + ":" + duracao.toMinutesPart() + ":" + duracao.toSecondsPart());
-
+        System.out.print("Idade:");
+        System.out.println(obrigatoriedade(scan.nextInt()));
         scan.close();
+    }
+
+    public static String obrigatoriedade(int idade) {
+        if (idade > 0 && idade <= 15) {
+            return ("Sem permissão para votar!");
+        } else if (idade > 15 && idade <= 17 || idade > 64) {
+            return ("Voto facultativo!");
+        } else if (idade >= 18 && idade <= 64) {
+            return ("Voto obrigatório!");
+        } else {
+            return ("Idade Inexistente");
+        }
     }
 
 }
