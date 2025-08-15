dbed154c6b9039d492a8cdbf16447060960564c2
Henrique Madureira <carloshenriquemadureira.santos@ucsal.edu.br>
2025-08-14 23:27:30 -0300

    Questão 10

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -3,32 +3,28 @@ package application;
 import java.util.Scanner;
 import java.util.Locale;
 
-// 9) Faça um programa que receba um valor n e em seguida informe o valor de S.
-// a) s=1+1/2+1/3 +...+ 1/n
-// b) s=1/n + 2/(n-1) + 3/(n-2) +...+ (n-1)/2+n
-// c) s=1 - 1/2 + 1/3 - 1/4 +...+ 1/n
+// 10)Faça um programa que receba um número e informe se é um termo da sequência de Fibonacci. Na
+// sequência de Fibonacci cada termo é a soma dos dois termos anteriores:
+// 1, 1, 2, 3, 5, 8, 13...
 
 public class Questions {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-
-        System.out.println("Qual o valor de n?");
-        int i = scan.nextInt();
-
-        double[] s = new double[3];
-
-        for (int n = 1; n < i; n++) {
-            s[0] += (1 + 1) / n;
-            s[1] += (n - 1) / 2 + n;
-            s[2] += 1 / n;
-        }
-
-        System.out.println("\nResultado");
-        System.out.println("a) " + String.format("%.2f", s[0]));
-        System.out.println("b) " + String.format("%.2f", s[1]));
-        System.out.println("c) " + String.format("%.2f", s[2]));
-
+        System.out.print("Quantos números o Fibonacci deve ter? ");
+        sequenciaDeFibonacci(scan.nextInt());
         scan.close();
+    }
+
+    public static void sequenciaDeFibonacci(int quantidade) {
+        int[] fibonacci = new int[quantidade];
+        fibonacci[2] = 1;
+        for (int i = 0; i < quantidade; i++) {
+            fibonacci[0] = fibonacci[1];
+            fibonacci[1] += fibonacci[2];
+            System.out.print(fibonacci[1] + ", ");
+            fibonacci[2] = fibonacci[0];
+        }
+        System.out.print("(...)");
     }
 
 }
